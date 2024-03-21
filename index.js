957a9e07b9ffdfbcee0af779223d2eb06471ac77
Nithin2104p <tnithin688@gmail.com>
2024-03-21 10:21:27 +0530

    made changes

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://canteen-evz0.onrender.com/images/${req.file.filename}`
     })
 })
 //shema for products
